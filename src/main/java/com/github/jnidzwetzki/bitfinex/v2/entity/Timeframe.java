7bd71ceeafa113cd63cc70c88f04661f4ab4553e
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2017-12-27 22:00:29 +0100

    Fixed timeframe

--- src/main/java/com/github/jnidzwetzki/bitfinex/v2/entity/Timeframe.java
+++ src/main/java/com/github/jnidzwetzki/bitfinex/v2/entity/Timeframe.java
@@ -8,7 +8,7 @@ public enum Timeframe {
 	MINUTES_1(TimeUnit.MINUTES.toMillis(1), "1m"),
 	MINUTES_5(TimeUnit.MINUTES.toMillis(5), "5m"),
 	MINUTES_15(TimeUnit.MINUTES.toMillis(15), "15m"),
-	MINUTES_30(TimeUnit.MINUTES.toMillis(3), "30m"),
+	MINUTES_30(TimeUnit.MINUTES.toMillis(30), "30m"),
 	HOUR_1(TimeUnit.HOURS.toMillis(1), "1h");
 	
 	private Timeframe(final long milliseconds, final String bitfinexString) {
