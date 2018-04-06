aad8dd45fa6de219d996e2af3e57cb3454ac8229
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-04-06 12:42:05 +0200

    Removed unused code

--- src/main/java/com/github/jnidzwetzki/bitfinex/v2/entity/APIException.java
+++ src/main/java/com/github/jnidzwetzki/bitfinex/v2/entity/APIException.java
@@ -24,9 +24,6 @@ public class APIException extends Exception {
 	 */
 	private static final long serialVersionUID = -6230854947150239935L;
 
-	public APIException() {
-	}
-
 	public APIException(final String message) {
 		super(message);
 	}
@@ -37,10 +34,6 @@ public class APIException extends Exception {
 
 	public APIException(final String message, final Throwable cause) {
 		super(message, cause);
-	}
-
-	public APIException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
 }
