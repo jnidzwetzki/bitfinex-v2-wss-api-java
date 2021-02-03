cd5dfe8807addd8cb8bee8b5bc75bfa594f2e0d2
wgolyakov <wolodia@narod.ru>
2021-02-03 15:01:56 +0300

    #210 Reduce code complexity in OrderNewCommand

--- src/main/java/com/github/jnidzwetzki/bitfinex/v2/command/OrderNewCommand.java
+++ src/main/java/com/github/jnidzwetzki/bitfinex/v2/command/OrderNewCommand.java
@@ -55,11 +55,15 @@ public class OrderNewCommand implements BitfinexOrderCommand {
         orderJson.put("cid", bitfinexOrder.getClientId());
         bitfinexOrder.getClientGroupId().ifPresent(groupId -> orderJson.put("gid", bitfinexOrder.getClientGroupId().get()));
         if (bitfinexOrder.getAffiliateCode() != null) {
-            final JSONObject metaJson = new JSONObject();
-            metaJson.put("aff_code", bitfinexOrder.getAffiliateCode());
-            orderJson.put("meta", metaJson);
+            orderJson.put("meta", getMetaJson());
         }
         return "[0, \"on\", null, " + orderJson.toString() + "]";
+    }
+
+    private JSONObject getMetaJson() {
+        final JSONObject metaJson = new JSONObject();
+        metaJson.put("aff_code", bitfinexOrder.getAffiliateCode());
+        return metaJson;
     }
 
 }
